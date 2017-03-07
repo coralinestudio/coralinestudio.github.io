16ea364833984fb9e59283352345c694924a9c94
coralinestudio <caroline.rcosta@hotmail.com>
2017-03-07 16:47:28 -0300

    Update canvas.js

--- _posts/canvas.js
+++ _posts/canvas.js
@@ -3,6 +3,10 @@ var canvas = document.getElementById('game');
 
 // Force canvas to dynamically change its size to
 
+var c = canvas.getContext('2d');
+
+c.fillStyle = '#000000';
+c.fillRect (0, 0, 20, 20);
 
 var phrase = "Click or tap the screen to start the game";
 c.font = 'bold 16px Arial, sans-serif';
